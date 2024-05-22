f745b128beea59235f84183276ba44ac657b0574
Cristhian <cristhian.camacho@gulfstream.com>
2024-05-22 16:57:34 -0700

    commit

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -1,54 +1,62 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import {CoursesCardListComponent} from './courses-card-list.component';
-import {CoursesModule} from '../courses.module';
-import {COURSES} from '../../../../server/db-data';
-import {DebugElement} from '@angular/core';
-import {By} from '@angular/platform-browser';
-import {sortCoursesBySeqNo} from '../home/sort-course-by-seq';
-import {Course} from '../model/course';
-import {setupCourses} from '../common/setup-test-data';
+import { async, ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+import { setupCourses } from '../common/setup-test-data';
+
+import { CoursesCardListComponent } from './courses-card-list.component';
+import { CoursesModule } from '../courses.module';
+
+import { COURSES } from '../../../../server/db-data';
+import { sortCoursesBySeqNo } from '../home/sort-course-by-seq';
+import { Course } from '../model/course';
 
 
 describe('CoursesCardListComponent', () => {
 
     let component: CoursesCardListComponent;
+
     let fixture: ComponentFixture<CoursesCardListComponent>;
-    let el: DebugElement;
+
+    let elem: DebugElement;
 
     beforeEach(waitForAsync(() => {
+
         TestBed.configureTestingModule({
             imports: [CoursesModule]
         })
-        .compileComponents()
-        .then(() => {
-
-            fixture = TestBed.createComponent(CoursesCardListComponent);
-            component = fixture.componentInstance;
-            el = fixture.debugElement;
-
-        });
+            .compileComponents()
+            .then(() => {
+                fixture = TestBed.createComponent(CoursesCardListComponent);
+                component = fixture.componentInstance;
+                elem = fixture.debugElement;
+            })
     }));
 
-    it('should create the component', () => {
+    it("should create the component", () => {
 
         expect(component).toBeTruthy();
 
     });
 
-    it('should display the course list', () => {
+
+    it("should display the course list", () => {
 
         component.courses = setupCourses();
 
         fixture.detectChanges();
 
-        const cards = el.queryAll(By.css(".course-card"));
+        const cards = elem.queryAll(By.css('.course-card'));
 
-        expect(cards).toBeTruthy("Could not find cards");
-        expect(cards.length).toBe(12, "Unexpected number of courses");
+        expect(cards).toBeTruthy('Could not find cards');
+
+        expect(cards.length).toBe(12, 'Unexpected number of courses');
+
 
     });
 
-    it('should display the first course', () => {
+
+    it("should display the first course", () => {
 
         component.courses = setupCourses();
 
@@ -56,18 +64,16 @@ describe('CoursesCardListComponent', () => {
 
         const course = component.courses[0];
 
-        const card = el.query(By.css(".course-card:first-child")),
-                title = card.query(By.css("mat-card-title")),
-                image = card.query(By.css("img"));
+        const card = elem.query(By.css('.course-card:first-child')),
+            title = card.query(By.css('mat-card-title')),
+            image = card.query(By.css('img'));
 
-        expect(card).toBeTruthy("Could not find course card");
-
+        expect(card).toBeTruthy('Could not find course card');
         expect(title.nativeElement.textContent).toBe(course.titles.description);
-
-        expect(image.nativeElement.src).toBe(course.iconUrl);
+        expect(image.nativeElement.src).toBe(course.iconUrl)
 
     });
 
-});
 
+});
 
--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,53 +1,50 @@
-import {CalculatorService} from './calculator.service';
-import {LoggerService} from './logger.service';
-import {TestBed} from '@angular/core/testing';
 
+import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
+import { TestBed } from '@angular/core/testing';
 
 describe('CalculatorService', () => {
+    let calculator: CalculatorService, loggerSpy: any;
 
-    let calculator: CalculatorService,
-        loggerSpy: any;
-
-    beforeEach(()=> {
-
-        console.log("Calling beforeEach");
-
-        loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);
+    beforeEach(() => {
+        console.log('Calling beforeEach');
+        loggerSpy = jasmine.createSpyObj('LoggerService', ["log"])
 
         TestBed.configureTestingModule({
             providers: [
                 CalculatorService,
-                {provide: LoggerService, useValue: loggerSpy}
-            ]
-        });
+                {provide: LoggerService, useValue:  loggerSpy}
+            ],
 
-        calculator = TestBed.inject(CalculatorService);
+        })
 
-    });
+        calculator = TestBed.inject(CalculatorService)
+    })
 
     it('should add two numbers', () => {
-
-        console.log("add test");
+        console.log('Add test');
 
         const result = calculator.add(2, 2);
 
         expect(result).toBe(4);
 
-        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1)
 
     });
 
-
     it('should subtract two numbers', () => {
 
-        console.log("subtract test");
+        console.log('Subtract test');
 
         const result = calculator.subtract(2, 2);
 
         expect(result).toBe(0, "unexpected subtraction result");
 
-        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1)
+
 
     });
+
+
 
 });
